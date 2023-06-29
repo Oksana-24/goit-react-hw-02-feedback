7101773285bc6006df7b5a2e13ebc82264a0c8f0
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-06-29 11:34:28 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,6 @@ import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
-
-
 export class App extends Component {
   state = {
     good: 0,
