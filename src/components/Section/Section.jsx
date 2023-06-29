c864dce764f1c876a5cae429128e450ab88494e5
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-06-29 11:34:36 +0200

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -2,15 +2,15 @@ import css from './Section.module.css';
 import PropTypes from 'prop-types';
 
 export const Section = ({ title, children }) => {
-    return (
-        <section>
-            <p className={css.sectionTitle}>{title}</p>
-            {children}
-        </section>
-    );
+  return (
+    <section>
+      <p className={css.sectionTitle}>{title}</p>
+      {children}
+    </section>
+  );
 };
 
 Section.propTypes = {
-    title: PropTypes.string.isRequired,
-    children: PropTypes.node.isRequired
-}
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
