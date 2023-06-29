e241882995723b210f7da44f767ac704bce53af3
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-06-29 11:34:31 +0200

    Update Feedback.jsx

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,22 +2,22 @@ import css from './Feedback.module.css';
 import PropTypes from 'prop-types';
 
 export const Feedback = ({ values, addFeedback }) => {
-    return (
-        <div class={css.buttonList}>
-            {values.map(value => (
-                <button
-                    type="button"
-                    key={value}
-                    onClick={() => addFeedback(value)}
-                    className={css.buttonItem}
-                >
-                    {value}
-                </button>
-            ))}
-        </div>
-    );
+  return (
+    <div class={css.buttonList}>
+      {values.map(value => (
+        <button
+          type="button"
+          key={value}
+          onClick={() => addFeedback(value)}
+          className={css.buttonItem}
+        >
+          {value}
+        </button>
+      ))}
+    </div>
+  );
 };
 Feedback.propTypes = {
-    values: PropTypes.arrayOf(PropTypes.string).isRequired,
-    addFeedback: PropTypes.func.isRequired,
+  values: PropTypes.arrayOf(PropTypes.string).isRequired,
+  addFeedback: PropTypes.func.isRequired,
 };
